d0d34efb48ed09aac10c88d5480eb0c880385b15
dharun-08 <dharun08kumar@gmail.com>
2023-10-30 14:28:27 +0530

    first committ

---
+++ hallowInvertedHalfPy.java
@@ -0,0 +1,15 @@
+/**
+ * hallowInvertedHalfPy
+ */
+public class hallowInvertedHalfPy {
+    public static void main(String[] args) {
+        int row = 5;
+        int column = 5;
+        for(int i=0;i<row;i++){
+            for(int j=i;j<column;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
